0e64a587b30ce97e56ffff33a64e2c915cd84f08
uckuruslukopera <loginquitas@gmail.com>
2018-07-28 18:19:18 +0300

    added angular package configs

--- system.config.js
+++ system.config.js
@@ -7,7 +7,12 @@ System.config({
         'core-js': 'npm:core-js',
         'zone.js': 'npm:zone.js',
         'rxjs': 'npm:rxjs',
-        'tslib': 'npm:tslib/tslib.js'
+        'tslib': 'npm:tslib/tslib.js',
+        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
     },
     packages: {
         'dist/app': {},
